2fea6074b29898ee6b20862b14566957f9f7df6e
unknown <nezabruce@gmail.com>
2022-02-11 11:50:21 -0800

    themovieanimated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Movieanim from './components/Movieanim';
+
+function App() {
+  return (
+    <div className="App">
+    <Movieanim/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Movieanim.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import Movie from './Movie';
+
+const Movieanim = () => {
+    const [popular,setPopular]=useState([]);
+    useEffect(()=>{
+fetchpopular();
+},[]);
+async function fetchpopular(){
+    const data=await fetch("https://api.themoviedb.org/3/movie/popular?api_key=335c7bfbb6b995d1c05eaf30942069e4&language=en-US&page=1");
+    // const data=await fetch("https://api.themoviedb.org/3/movie/9?api_key=335c7bfbb6b995d1c05eaf30942069e4&language=en-US");
+    const movies=await data.json();
+    setPopular(movies.results);
+    // console.log(movies.results)
+    
+    };
+  return (
+    <div className="popular">
+        {popular.map((movie)=>{
+     return <Movie key={movie.id} movie={movie}/>
+        // <div>{movie}</div>
+    })}
+    </div>
+  )
+}
+
+export default Movieanim
