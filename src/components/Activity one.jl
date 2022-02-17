d068247bc19e5099b86d076a3d32fb44846e5308
nezabruce@gmail.com <NezaBruce>
2022-02-17 21:21:04 +0200

    ACT1

---
+++ src/components/Activity one.jl
@@ -0,0 +1,105 @@
+Activity one
+
+1.s,k,s,|S|,tf
+g,dz,g,g
+ae,ae,ei,ae
+s,z,Z,z
+I,aI,ii,ae
+^,^,?,U:
+K,k,K,k
+ei
+s
+
+B
+
+tax (ks)
+thing (Q)
+schedule(S)
+school(sk)
+are(a:r)
+though(ou)
+chrome(kr)
+
+2. ii               
+
+1 salmon
+climb
+yawn
+joke
+further
+onion
+opera
+creation
+confusion
+ask
+
+
+vowels
+|e|: bell
+|?|:father
+|z:|bird
+|0|:dog
+|)|:law
+|U|:book
+|u:|food
+
+|ei|:say
+|ai|:high
+|)i|:boy
+|au|:so/no
+|au|:now
+|i:|:steam,sleep
+|I|:system,busy
+|e|:bread,said
+|a|:|ae|:stamp,flat
+|a:|heart,path
+|z:|:/|e)|:   shirt 
+|D|:hot,cough,
+|):|:poor,saw
+|U|:    should
+|u:|:shoe,flew,wood
+|^|:fload,does
+|x)|/|z|:scha sound:turn,teach|ts|:er,asleep
+voiceds sound consonant
+sibilant letters
+
+
+
+
+
+
+
+3.
+heart,
+bed
+pool;
+caught
+voice
+where
+chair
+brown
+worried
+
+
+
+
+
+
+au>found
+bee>i:
+blue>u:
+learn>3:
+ugly>^
+tray>ei
+caught>au,ought,mouse
+soup>u:june
+flight>ai
+ill>i:
+sixty>I,silly
+worry>^
+white>ai
+shirt>3:
+jus>^
+fee>beee>each
+tale,tray,hey,eight
+tur,shirt,bird
