bc296bee1f7bc0d791ac9ec9c918a1af2eb6ced0
NezaBruce <nezabruce@gmail.com>
2022-02-20 19:06:15 +0200

    sundend

--- src/components/filter.js
+++ src/components/filter.js
@@ -2,10 +2,10 @@ import React, { useEffect } from 'react'
 // setFiltered={setFiltered} setActiveId={setActiveId} activeId={activeId} popular={popular}
 function Filter  ({setFiltered,setActiveId,activeId,popular}){
   useEffect(()=>{
-    if(activeId === 0){
-    setFiltered(popular);
-    return;
-    }
+    // if(activeId === 0){
+    // setFiltered(popular);
+    // return;
+    // }
   const filtera=popular.filter((filt)=>{
     filt.genre_ids.includes(activeId);
     // console.log(filt.genre_ids);
@@ -18,11 +18,7 @@ function Filter  ({setFiltered,setActiveId,activeId,popular}){
       <button className={activeId === 0 ? 'active' : ""} onClick={()=>setActiveId(0)}>All</button>  
       <button className={activeId === 35 ? 'active' : ""} onClick={()=>setActiveId(35)}>Comedy</button>  
       <button className={activeId === 28 ? 'active' : ""} onClick={()=>setActiveId(28)}>Action</button>  
-      <button className={activeId === 0 ? 'active' : ""} onClick={()=>setActiveId(0)}>All</button>  
-      <button className={activeId === 35 ? 'active' : ""} onClick={()=>setActiveId(35)}>Comedy</button>  
-      <button className={activeId === 28 ? 'active' : ""} onClick={()=>setActiveId(28)}>Action</button>  
     </div>
- 
   )
 }
 export default Filter
